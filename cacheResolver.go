7c11e4e819197ac7fad8f4eaf9cd5ec1a1296d85
Alikhan <alikhan@appomart.com>
2025-02-22 18:51:46 +0600

    Adding TTL functionality to cache resolver pachage

--- cacheResolver.go
+++ cacheResolver.go
@@ -1,16 +1,45 @@
 package cacheResolver
 
-type CacheResolver map[string]interface{}
+import (
+	"sync"
+	"time"
+)
 
-func (cache CacheResolver) Get(key string) (interface{}, bool) {
-	val, ok := cache[key]
+type CacheResolver struct {
+	mut   sync.Mutex
+	cache map[string]interface{}
+}
+
+func NewCacheResolver() *CacheResolver {
+	return &CacheResolver{
+		cache: make(map[string]interface{}),
+	}
+}
+
+func (c *CacheResolver) Get(key string) (interface{}, bool) {
+	c.mut.Lock()
+	defer c.mut.Unlock()
+	val, ok := c.cache[key]
 	return val, ok
 }
 
-func (cache CacheResolver) Put(key string, val interface{}) {
-	cache[key] = val
+func (c *CacheResolver) Put(key string, val interface{}) {
+	c.mut.Lock()
+	defer c.mut.Unlock()
+	c.cache[key] = val
 }
 
-func (cache CacheResolver) Del(key string) {
-	delete(cache, key)
+func (c *CacheResolver) Del(key string) {
+	c.mut.Lock()
+	defer c.mut.Unlock()
+	delete(c.cache, key)
+}
+
+func (c *CacheResolver) Set(key string, value interface{}, ttl time.Duration) {
+	c.Put(key, value)
+
+	go func() {
+		time.Sleep(ttl)
+		c.Del(key)
+	}()
 }
